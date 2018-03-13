9fc6460af3402dfeb55a16a887d620dae7455a94
Dikaeinstein <Dikaeinstein@users.noreply.github.com>
2018-03-13 23:41:28 +0100

    updated directory structure

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -3,31 +3,37 @@ import ProjectCard from './ProjectCard';
 
 const projects = [
   {
+    id: 0,
     name: 'Local Weather App',
     imgUrl: 'portfolio/weather-proj_modqed',
     url: 'https://dikaeinstein.github.io/local_weather_app',
   },
-  {
+  { 
+    id: 1,
     name: 'Styles Conference',
     imgUrl: 'portfolio/resp-proj_x9x7wx',
     url: 'https://dikaeinstein.github.io/styles_conference',
   },
   {
+    id: 2,
     name: 'Styles Conference Registration',
     imgUrl: 'portfolio/resp2-proj_twvv9d',
     url: 'https://dikaeinstein.github.io/styles_conference/register',
   },
   {
+    id: 3,
     name: 'Tic Tac Toe',
     imgUrl: 'portfolio/tic-tac-toe-proj_m4ywk2',
     url: 'https://dikaeinstein.github.io/tic-tac-toe',
   },
   {
+    id: 4,
     name: 'Calculator',
     imgUrl: 'portfolio/calc-proj_crhkbm',
     url: 'https://dikaeinstein.github.io/calculator',
   },
   {
+    id: 5,
     name: 'Random Quote Machine',
     imgUrl: 'portfolio/quote_proj_revcsz',
     url: 'https://dikaeinstein.github.io/random_quote',
@@ -42,6 +48,7 @@ const Portfolio = () => (
       </header>
       {projects.map(project => (
         <ProjectCard
+          key={project.id}
           imgUrl={project.imgUrl}
           name={project.name}
           url={project.url}
